3b33d53056fe03463af8134173cb9a58c73327a3
Chiazokam <chiazokamecheta@gmail.com>
2018-10-27 13:10:02 +0100

    Add css sheet, wtf-form and semantic ui

--- flaskapp.py
+++ flaskapp.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, session, request, jsonify
+from wtforms import Form, validators, StringField, TextAreaField, DateField, IntegerField, SelectField, SubmitField
 
 app = Flask(__name__)
-#moment = Moment(app)
+
+class ExpenseForm(Form):
+    expense_date = DateField('Date: yyyy-mm-dd', [validators.DataRequired()])
+    expense_descr = StringField('Description', [validators.length(max=20), validators.DataRequired()])
+    expense_amt = IntegerField('Amount', [validators.DataRequired()])
+    expense_cat = StringField('Category: business Or personal', [validators.DataRequired()])
+
 
 @app.route('/')
 def index():
-    return render_template('hello.html')
+    form = ExpenseForm(request.form)
+    return render_template('hello.html', form=form)
 
 #=========================================
 if __name__ == '__main__':
