f697c6e6d0255580bb6fc1bf59231b1a572d0214
Chiazokam <chiazokamecheta@gmail.com>
2018-10-27 16:13:18 +0100

    Connect to the database

--- flaskapp.py
+++ flaskapp.py
@@ -1,7 +1,26 @@
 from flask import Flask, render_template, url_for, redirect, session, request, jsonify
 from wtforms import Form, validators, StringField, TextAreaField, DateField, IntegerField, SelectField, SubmitField
+import mysql.connector as mariadb
+from datetime import datetime
+import pymysql  #Downloaded and imported
 
 app = Flask(__name__)
+
+def dbConnection():
+    """Function to connect to the DB"""
+    db_conn = mariadb.connect(user='root',
+                              password='root',
+                              database='kuditracker')
+    cursor = db_conn.cursor()
+
+#Create function to change date to a date object
+def date_format(date_string):
+    """Function to return date object"""
+    try:
+        return datetime.strptime(date_string, "%Y-%m-%d").date()
+    except ValueError:
+        raise ValueError("Incorrect date format, should be in yyyy-mm-dd format")
+
 
 class ExpenseForm(Form):
     expense_date = DateField('Date: yyyy-mm-dd', [validators.DataRequired()])
@@ -10,9 +29,29 @@ class ExpenseForm(Form):
     expense_cat = StringField('Category: business Or personal', [validators.DataRequired()])
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     form = ExpenseForm(request.form)
+
+    if request.method == 'POST':
+        expense_date = date_format(request.form['expense_date'])
+        #Escape characters that may cause an error when inserted into the db e.g, apostrophe
+        expense_descr =  pymysql.escape_string(request.form['expense_descr'])
+        expense_amt = pymysql.escape_string(request.form['expense_amt'])
+        expense_cat = request.form['expense_cat']
+
+        #Connect to mariaDB
+        db_conn = mariadb.connect(user='root',
+                                      password='root',
+                                      database='kuditracker')
+        cursor = db_conn.cursor()
+        #Insert data into the db
+        query = "INSERT INTO expenses (ExpenseDate, Description, Amount, Category) VALUES ('{}', '{}', '{}', '{}')".format(expense_date, expense_descr, expense_amt, expense_cat)
+        cursor.execute(query)
+        db_conn.commit()
+        #Close connection
+        cursor.close()
+        return redirect(url_for("index"))
     return render_template('hello.html', form=form)
 
 #=========================================
